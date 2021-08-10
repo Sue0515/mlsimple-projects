5af044a9b3ac467b9f6249437db3d1b94598ab3b
SuinCho <suin.cho@stonybrook.edu>
2021-08-11 01:25:27 +0900

    added python script that predicts survival rate of patient

---
+++ death_prediction.py
@@ -0,0 +1,19 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+import numpy as np
+import tensorflow as tf
+
+np.random.seed(3)
+tf.random.set_seed(3)
+
+dataset = np.loadtxt("../datasets/ThoraricSurgery.csv", delimeter=",")
+X = dataset[:,0:17]
+Y = dataset[:,17]
+
+model = Sequential()
+model.add(Dense(30, input_dim=17, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X, Y, epochs=100, batch_size=10)
